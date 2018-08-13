a09ab4fbd90f3ce74abebfa10cf27849f9e289ce
Mohsin Shafqat <mohsin.paracha88@gmail.com>
2018-08-13 17:44:33 +0800

    m

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound =  () => {
+  return (
+    <div>
+     <h1 className="display-4"> <span className = "text-danger ">404</span> Page not found</h1>
+     <p>Sorry, This page doesnt exist!</p>
+    </div>
+  )
+}
+export default NotFound;
